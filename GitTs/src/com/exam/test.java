4195a42b543c8c4c616099a9de45423fb7138a78
smhrd <smhrd@211.223.37.217>
2021-11-10 16:32:55 +0900

    세번째 커밋!

--- GitTs/src/com/exam/test.java
+++ GitTs/src/com/exam/test.java
@@ -3,13 +3,9 @@ package com.exam;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Git 테스트!");
 		System.out.println("두번째 커밋!");
 		System.out.println("세번째 커밋!");
-		System.out.println("정배의 커밋");
-		System.out.println("정식의 커밋");
-		System.out.println("커밋들어간다잇 아가리딱벌려라잇~!!!!!!");
 	}
 
 }
---
+++ GitTs/src/com/exam/test02.java
@@ -0,0 +1,9 @@
+package com.exam;
+
+public class test02 {
+
+	public static void main(String[] args) {
+		System.out.println("세 번째 커밋!");
+	}
+
+}
