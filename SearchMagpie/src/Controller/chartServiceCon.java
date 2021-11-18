ca1270311bcf100047c7c909dd611ac08ae8fc20
smhrd <smhrd@222.102.104.28>
2021-11-18 11:09:34 +0900

    ChartDAO 차트 관련 DAO생성  1.DB연결 메소드 생성  2.DB연결 끊는 메소드 생성  3.조회수 1증가 메소드 생성  (DB에 접근해서 t_product 테이블의 p_view_cnt 컬럼  즉, 조회수를 1늘려주는 sql문을 전송하는 메소드)  4.조회수를 가지고 오는 메소드 생성  (DB에  t_product 테이블의 p_view_cnt 컬럼  의 현재 조회수를 가지고 오는 메소드  파라미터로 제품 고유 번호인 p_seq 를 입력하면 해당 제품의 조회수를    가지고 옴)
    chartDTO 생성
    1. DB t_product 테이블의 컬럼과 일치하게 DTO 생성
    2. getter, setter 생성
    3. toString 생성
    !주의 : DB에는 date로 설정된 값을 String 자료형으로 DTO에서 담아냄
    
    chartServicecon 생성
    서비스 콘 내부에 주석으로 해야 할 일 메모
    
    서비스 콘은 아직 작성하기 이릅니다

---
+++ SearchMagpie/src/Controller/chartServiceCon.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/chartServiceCon")
+public class chartServiceCon extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1. DB t_product로부터 제품 고유의 seq 번호를 받아온다.
+		//2. chartDAO의 getViewCount(seq번호 입력)
+		//3. DTO로부터 해당 제품 카테고리,이름,가격,사진을 가지고 온다.
+		//4. 가지고 온 정보를 뷰로 보낸다
+		
+		//2021.11.18 10:45 현재 시각 제품들이 DB에 아직 저장이 
+		//안 된 상태라 지금 진행하기엔 다소 어려움
+	}
+
+}
