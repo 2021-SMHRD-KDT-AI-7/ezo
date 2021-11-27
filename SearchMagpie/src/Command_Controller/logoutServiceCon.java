023c3143541183cc5167d1e189aa2ca08f580fa8
smhard <smhrd@222.102.104.28>
2021-11-27 10:13:24 +0900

    asd

---
+++ SearchMagpie/src/Command_Controller/logoutServiceCon.java
@@ -0,0 +1,16 @@
+package Command_Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Interface.Command;
+
+public class logoutServiceCon implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
